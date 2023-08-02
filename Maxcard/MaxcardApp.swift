e2748ab59da207075fdc8e618dc1ad465eb42109
war823 <maximkovol@gmail.com>
2023-08-02 19:33:31 +0200

    Initial Commit

---
+++ Maxcard/MaxcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxcardApp.swift
+//  Maxcard
+//
+//  Created by Max Volokhovskyi on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MaxcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
